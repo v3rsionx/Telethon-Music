5ebb1b531dcb97518786d04fbad49cc8e49fe43c
v3rsionx <144494230+v3rsionx@users.noreply.github.com>
2023-09-09 01:47:03 +0300

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,8 +1,8 @@
 import os
 
 class Config(object):
-    API_ID = int(os.environ.get("API_ID", "6213538"))
-    API_HASH = os.environ.get("API_HASH", "8ce3522bd21cc937eee4c68813d501d5")
+    API_ID = int(os.environ.get("API_ID", "29405343"))
+    API_HASH = os.environ.get("API_HASH", "21cf17b612d3e12d02a8af0bef657759")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
     STRING_SESSION = os.environ.get("STRING_SESSION", None)
     MANAGEMENT_MODE = os.environ.get("MANAGEMENT_MODE", None)
